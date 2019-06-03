a0199f2124a003e6b241f5f185c69be8dade7fa7
Veerle van der Tas <veerle@vdtas.com>
2019-06-03 15:30:23 +0200

    Extracting printHelper

--- src/nn.jl
+++ src/nn.jl
@@ -8,6 +8,8 @@ using ConditionalJuMP
 
 import JSON
 
+include("helper/printHelper.jl")
+
 """
 Add constraints for neural network, then optimize.
 """
@@ -80,71 +82,8 @@ function modelNN(input, w, b, label, layerSizes, doAdversarial)
   return m,x,s
 end
 
-"""
-Print decision variable 'val' with name 'name'
-"""
-function printDecLayers(name, val, range, layerSizes)
-  println("$name Values: ")
-  for k in range
-    println("Layer ", k, ':')
-    for j in 1:layerSizes[k]
-      print("$(@sprintf("%.2f",getvalue(val[k,j]))) ")
-    end
-    println(" ")
-  end
-  println("-------------")
-end
 
-"""
-Print regular variable 'val' with name 'name'
-"""
-function printLayers(name, val, range, layerSizes)
-  println("$name: ")
-  for k in range
-    println("Layer ", k, ':')
-    tmp = val[string(k)]
-    for i in 1:size(tmp,1)
-      for j in 1:size(tmp,2)
-        print("$(@sprintf("%.2f",tmp[i,j])) ")
-      end
-      println(" ")
-    end
-  end
-  println("-------------")
-end
 
-function getPredictedLabel(m,x,layerSizes)
-  numLayers = size(layerSizes,1)
-  maxVal = findmax([getvalue(x[numLayers,j]) for j in 1:layerSizes[numLayers]])
-  label = maxVal[2] - 1
-  return label
-end
-
-"""
-Print all variables
-"""
-function printVars(m,x,s,w,b,layerSizes,label,predLabel,printWeights)
-
-  numLayers = size(layerSizes,1)
-  
-  println(" ")
-  println("Objective Value: ", getobjectivevalue(m))
-  println("Time: ", getsolvetime(m))
-  println(" ")
-
-  printDecLayers("X", x, 2:numLayers, layerSizes)
-  printDecLayers("S", s, 2:numLayers, layerSizes)
-  if printWeights
-    printLayers("Weights", w, 1:numLayers-1, layerSizes)
-    printLayers("Biases", b, 1:numLayers-1, layerSizes)
-  end
-  println("====================================")
-  modelPredLabel = getPredictedLabel(m,x,layerSizes)
-  println("True label: $label")
-  println("NN Predicted Label: $predLabel")
-  println("Model Predicted Label: $modelPredLabel")
-  println(" ")
-end
 
 """
 Load data from json file 'file'
